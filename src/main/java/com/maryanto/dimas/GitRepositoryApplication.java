df1c16d01806eb89d44c5f46a84fa99d9bb13219
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-25 14:38:53 +0700

    init project

---
+++ src/main/java/com/maryanto/dimas/GitRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepositoryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/maryanto/dimas/api/GitApi.java
@@ -0,0 +1,36 @@
+package com.maryanto.dimas.api;
+
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/git")
+public class GitApi {
+
+    @PostMapping("createProject/{projectName}")
+    public ResponseEntity createProject(@PathVariable("projectName") String projectName) {
+        String gitRepository = new StringBuilder(System.getProperty("user.home"))
+                .append(File.separator).append(projectName).append(File.separator).append(".git").toString();
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        try {
+            Repository repository = builder.setGitDir(new File(gitRepository))
+                    .readEnvironment() // scan environment GIT_* variables
+                    .findGitDir() // scan up the file system tree
+                    .build();
+            repository.create(true);
+            return new ResponseEntity(repository.getDirectory().getAbsolutePath(), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+    }
+}
