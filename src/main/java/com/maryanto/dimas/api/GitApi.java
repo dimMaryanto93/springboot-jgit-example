3aaebdd61a93b0faa3b958868c88e228bb48f0fb
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-07-04 11:26:46 +0700

    update membuat diff formater

--- src/main/java/com/maryanto/dimas/api/GitApi.java
+++ src/main/java/com/maryanto/dimas/api/GitApi.java
@@ -8,8 +8,15 @@ import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.AbstractTreeIterator;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +27,7 @@ import org.springframework.web.context.annotation.RequestScope;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -142,6 +150,86 @@ public class GitApi {
         } catch (GitAPIException e) {
             e.printStackTrace();
             return new ResponseEntity(HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/diff/{projectName}")
+    public ResponseEntity listDiff(
+            @PathVariable("projectName") String projectName,
+            @RequestParam(name = "oldCommit") String oldCommit,
+            @RequestParam(name = "newCommit") String newCommit) {
+        try {
+            Repository repository = properties.getRepository(projectName);
+            Git gitCommand = new Git(repository);
+            List<DiffEntry> diffEntries = listDiff(repository, gitCommand, oldCommit, newCommit);
+            StringBuilder sb = new StringBuilder();
+            for (DiffEntry entry : diffEntries) {
+                sb.append(entry.getChangeType().toString())
+                        .append(" : ")
+                        .append(
+                                entry.getOldPath().equals(entry.getNewPath()) ? entry.getNewPath() : entry.getOldPath()
+                                        + " -> " + entry.getNewPath()
+                        );
+
+                OutputStream output = new OutputStream() {
+                    StringBuilder builder = new StringBuilder();
+
+                    @Override
+                    public void write(int b) throws IOException {
+                        builder.append((char) b);
+                    }
+
+                    public String toString() {
+                        return this.builder.toString();
+                    }
+                };
+
+                try (DiffFormatter formatter = new DiffFormatter(output)) {
+                    formatter.setRepository(repository);
+                    formatter.format(entry);
+                }
+                sb.append("\n").append(output.toString());
+            }
+            return new ResponseEntity(sb.toString(), HttpStatus.OK);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    private static List<DiffEntry> listDiff(Repository repository, Git git, String oldCommit, String newCommit) throws GitAPIException, IOException {
+        final List<DiffEntry> diffs = git.diff()
+                .setOldTree(prepareTreeParser(repository, oldCommit))
+                .setNewTree(prepareTreeParser(repository, newCommit))
+                .call();
+
+        System.out.println("Found: " + diffs.size() + " differences");
+        for (DiffEntry diff : diffs) {
+            System.out.println("Diff: " + diff.getChangeType() + ": " +
+                    (diff.getOldPath().equals(diff.getNewPath()) ? diff.getNewPath() : diff.getOldPath() + " -> " + diff.getNewPath()));
+        }
+        return diffs;
+    }
+
+    private static AbstractTreeIterator prepareTreeParser(Repository repository, String objectId) throws IOException {
+        // from the commit we can build the tree which allows us to construct the TreeParser
+        //noinspection Duplicates
+        try (RevWalk walk = new RevWalk(repository)) {
+            RevCommit commit = walk.parseCommit(repository.resolve(objectId));
+            RevTree tree = walk.parseTree(commit.getTree().getId());
+
+            CanonicalTreeParser treeParser = new CanonicalTreeParser();
+            try (ObjectReader reader = repository.newObjectReader()) {
+                treeParser.reset(reader, tree.getId());
+            }
+
+            walk.dispose();
+
+            return treeParser;
         }
     }
 
